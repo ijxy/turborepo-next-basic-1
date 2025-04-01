404a01f2b4f6d96a9f56a1e142ad0ae2ed4ed2a9
ijxy <32483798+ijxy@users.noreply.github.com>
2025-04-01 17:37:42 +0100

    only affects apps/web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1>WEB</h1>
         <ThemeImage
           className={styles.logo}
           srcLight="turborepo-dark.svg"
